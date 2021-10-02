510a0f4a75bf46f1cab7ff49d2adab51884770bd
bihari <72039366+neelay-naman@users.noreply.github.com>
2021-10-02 13:55:30 +0530

    Update wallpaper-changer.py

--- wallpaper-changer.py
+++ wallpaper-changer.py
@@ -1,14 +1,17 @@
 import time
 import os
 
+#Specify a folder path here you've stored the wallpapers
 path_of_folder = "/media/neelay/LinuxPart/Wallpapers/"
 
 while(True):
 
     for file in os.listdir(path_of_folder):
         path_of_file = os.path.join(path_of_folder,file)
-
+		
+		#The Linux command to change the wallpaper
         cmd = f'gsettings set org.gnome.desktop.background picture-uri file:////{path_of_file}'
         os.system(cmd)
+		
+		#Wallpaper refreshes every 10 minutes, 10*60=600 seconds
         time.sleep(600)
-
